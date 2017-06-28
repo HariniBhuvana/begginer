ba6905d94d1f60cad807e098fff289cf5a9b0e0a
Harini <harinee2015@gmail.com>
2017-06-28 22:13:53 +0530

    Create Character.java

---
+++ Character.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.util.8;
+public class Character
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+char a=s.nextLine();
+if(a!=b[i])
+{
