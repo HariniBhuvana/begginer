b3973eeb25cd79357e5ecdb9f273cb18d1b727e6
Harini <harinee2015@gmail.com>
2017-06-28 21:47:16 +0530

    Create Case.java

---
+++ Case.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.*;
+public class Case
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int n=s.nextInt();
+if(n>0)
+{
+System.out.println("Positive");
+}
+else if(n<0)
+{
+system.out.println("Negative");
+}
+else
+{
+System.out.println("Zero");
+}
+}
+}
