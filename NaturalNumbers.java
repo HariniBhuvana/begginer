ba27db71ee435555a79bf20fa14f2be82da56dfb
Harini <harinee2015@gmail.com>
2017-07-03 15:58:06 +0530

    Update NaturalNumbers.java

--- NaturalNumbers.java
+++ NaturalNumbers.java
@@ -2,10 +2,23 @@ import java.io.*;
 import java.util.*;
 public class NaturalNumbers
 {
-public static void main(String args[])
-{
-Scanner s=new Scanner(System.in);
-int s=nextInt();
-for(int i=0;i<n;i++)
-{
-int a[]
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    int a[]=new int[n];
+    for(int i=0;i<n;i++)
+    {
+    
+     
+       a[i]=s.nextInt();
+    }
+    int sum=0;
+    for(int i=0;i<n;i++)
+    {
+      sum=sum+a[i];
+    }
+    System.out.println(sum);
+    
+  }
+}
