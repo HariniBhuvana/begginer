57e9f30d3941bd9eafb294291084ff132d9bce8b
Harini <harinee2015@gmail.com>
2017-07-03 14:41:40 +0530

    Create Large.java

---
+++ Large.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class Large
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+int a=s.nextInt();
+int b=s.nextInt();
+int c=s.nextInt();
+int max=0,temp;
+if(a>max)
+{
+max=a;
